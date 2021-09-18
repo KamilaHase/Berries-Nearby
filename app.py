9fae6036dcc8332c5e5dbe01c35a81a9282cef50
KamilaHase <k.rabinakova@gmail.com>
2021-09-18 14:26:27 +0000

    Added register template, styled. Footer not working, deleted footer for now.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,7 +23,12 @@ mongo = PyMongo(app)
 def get_offers():
     offers = mongo.db.offers.find()
     return render_template("offers.html", offers=offers)
-                            
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")                            
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
